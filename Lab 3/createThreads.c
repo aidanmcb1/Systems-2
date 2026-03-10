b94f7c89d574c0c5374ee522982f43d4568ff861
Aidan McBride <53662000+aidanmcb1@users.noreply.github.com>
2026-03-10 17:54:04 -0400

    makefile and part 1

--- Lab 3/createThreads.c
+++ Lab 3/createThreads.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <stdlib.h>
 #define THREAD_NO 10
 
 void *mythread(void *arg) {
     int *id = (int *)arg;
     printf("my id is %d\n", *id);
+    free(id);
+    return NULL;
 }
 
 int main(){
     pthread_t p[THREAD_NO];
     int i = 0;
     for(i=0; i<THREAD_NO; i++){
-        pthread_create(&p[i], NULL, mythread, &i);
+        int* id = malloc(sizeof *id);
+        *id = i;
+        pthread_create(&p[i], NULL, mythread, id);
     }
-
     for(i=0; i<THREAD_NO; i++){
 	pthread_join(p[i], NULL);
     }
